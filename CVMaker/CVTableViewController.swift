da1f1afca0938a639b118e9bf664b4bc7b5662d5
mahyar.zhiani <mahyar.janan@gmail.com>
2022-05-09 11:23:29 +0430

    create base CVTableViewController

---
+++ CVMaker/CVTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CVTableViewController.swift
+//  CVMaker
+//
+//  Created by Mahyar Jananeh on 5/9/22.
+//
+
+import UIKit
+
+class CVTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+}
