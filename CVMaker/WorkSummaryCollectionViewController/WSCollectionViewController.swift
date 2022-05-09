f1984a6d2e9f1ec102cce1207afa0f3d97e79cde
mahyar.zhiani <mahyar.janan@gmail.com>
2022-05-09 14:33:57 +0430

    create workSummary page viewCOntroller

---
+++ CVMaker/WorkSummaryCollectionViewController/WSCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WSCollectionViewController.swift
+//  CVMaker
+//
+//  Created by Mahyar Jananeh on 5/9/22.
+//
+
+import UIKit
+
+class WorkSummaryCollectionViewController: BaseCollectionViewController {
+    
+    
+    
+}
---
+++ CVMaker/WorkSummaryCollectionViewController/WSLifeCycle.swift
@@ -0,0 +1,19 @@
+//
+//  WSLifeCycle.swift
+//  CVMaker
+//
+//  Created by Mahyar Jananeh on 5/9/22.
+//
+
+import UIKit
+
+
+extension WorkSummaryCollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
