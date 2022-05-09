f7416f36dae64fe8be007aca013aeaeffa2c5257
mahyar.zhiani <mahyar.janan@gmail.com>
2022-05-09 13:33:53 +0430

    setup navigationBar on tableView

--- CVMaker/CVTableViewController.swift
+++ CVMaker/CVTableViewController.swift
@@ -14,6 +14,7 @@ class CVTableViewController: UITableViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        setupNavigationBar()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -23,6 +24,15 @@ class CVTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         return cell
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .systemBlue
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
+                                                                   NSAttributedString.Key.font: UIFont.myBoldSystemFont(ofSize: 16)]
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.title = Constants.Main.editCV
     }
     
 }
