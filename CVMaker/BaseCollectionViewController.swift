22802f4404fa5db5a7692d205c1cba0c27b77bf1
mahyar.zhiani <mahyar.janan@gmail.com>
2022-05-09 14:18:01 +0430

    create BaseCollectionViewController

---
+++ CVMaker/BaseCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseCollectionViewController.swift
+//  CVMaker
+//
+//  Created by Mahyar Jananeh on 5/9/22.
+//
+
+import UIKit
+
+class BaseCollectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(white: 0.9, alpha: 0.9)
+    }
+    
+}
