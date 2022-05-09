77f00fef25fc108b03f4448531985ce4c4f05c5d
mahyar.zhiani <mahyar.janan@gmail.com>
2022-05-09 14:22:36 +0430

    setup navigationBar

--- CVMaker/BaseCollectionViewLifeCycle.swift
+++ CVMaker/BaseCollectionViewLifeCycle.swift
@@ -17,4 +17,31 @@ extension BaseCollectionViewController {
         setupView()
     }
     
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .systemBlue
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
+                                                                   NSAttributedString.Key.font: UIFont.myBoldSystemFont(ofSize: 16)]
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        //        navigationItem.title = Constants.Main.workSummary
+        
+        createAddBarButton()
+    }
+    
+    private func createAddBarButton() {
+        let saveButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.save,
+                                             target: self,
+                                             action: #selector(saveButtonSelected))
+        let addButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.compose,
+                                            target:             self,
+                                            action:             #selector(addButtonSelected))
+        navigationItem.setRightBarButtonItems([saveButtonItem, addButtonItem], animated: true)
+    }
+    
+    @objc func addButtonSelected() {
+    }
+    @objc func saveButtonSelected() {
+    }
+    
 }
