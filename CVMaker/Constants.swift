81b89e2c6aebb99be3f466ac69857691ee68143e
mahyar.zhiani <mahyar.janan@gmail.com>
2022-05-09 10:58:25 +0430

    add default font to entire app

---
+++ CVMaker/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  CVMaker
+//
+//  Created by Mahyar Jananeh on 5/9/22.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum AppFontName {
+        static let regular  = "GillSans"
+        static let bold     = "GillSans-Bold"
+        static let italic   = "GillSans-Italic"
+    }
+    
+}
