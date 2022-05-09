6f8490cccfc3f218993a39614afaff4e6388aa1e
mahyar.zhiani <mahyar.janan@gmail.com>
2022-05-09 14:29:02 +0430

    setup CollectionView FlowLayout delegates

--- CVMaker/BaseCollectionViewControllerDelegates.swift
+++ CVMaker/BaseCollectionViewControllerDelegates.swift
@@ -12,3 +12,22 @@ extension BaseCollectionViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     }
 }
+
+
+// MARK: - UICollectionView Delegate FlowLayout
+extension BaseCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    // height of every cell of the collectionView
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 8, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 4, left: 12, bottom: 4, right: 12)
+    }
+    
+}
